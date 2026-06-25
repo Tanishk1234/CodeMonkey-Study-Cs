fd839f6f0a8dc8b024a91017d5cb88d3d708af4c
Tanishk1234 <72665545+Tanishk1234@users.noreply.github.com>
2026-06-25 13:16:38 +0530

    Create Variables class with data type examples
    Added a Variables class with examples of different data types.

---
+++ CodeMonkey-Study-C#/Variables.cs
@@ -0,0 +1,22 @@
+namespace CodeMonkey_Study_C_
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            
+            // Variables is kind of a container which can help us to store data in it 
+            // int age = 5; // int stores whole numbers
+            // float height = 5.9f; // float stores decimal numbers and after value is cumpolsory to add f at the end of the value
+            // double weight = 70.5; // double stores decimal numbers and it can store more decimal numbers than float
+            // string name = "John"; // string stores text
+            // bool isAlive = true; // bool stores true or false
+            // char gender = 'M'; // char stores a single character
+            // var isAlive = true; // var can store any type of data and it will automatically detect the type of data
+
+
+            //  Console.WriteLine($"my name is {name} and i am {age} years old"); //it uses string interpolation in this you dont want to seprate variavles again and again
+            
+        }
+    }
+}
